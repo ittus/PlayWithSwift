d33c6aba8fe0a00900ccfd6bb2f401f70f0d448d
ittus <vuminhthang.cm@gmail.com>
2016-05-11 11:17:17 +0800

    start Calculator

---
+++ Calculator/Calculator/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Calculator
+//
+//  Created by Minh Thang Vu on 5/11/16.
+//  Copyright © 2016 Minh Thang Vu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
