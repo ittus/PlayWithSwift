88d5e8a8c3de0f1818626c5c84cbe7c09c16876e
ittus <vuminhthang.cm@gmail.com>
2016-05-14 15:40:32 +0800

    finish calculator

--- Calculator/Calculator/FirstViewController.swift
+++ Calculator/Calculator/FirstViewController.swift
@@ -8,11 +8,48 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITextFieldDelegate {
 
+    // addition outlet
+    @IBOutlet weak var txtAddFirstNumber: UITextField!
+    @IBOutlet weak var txtAddSecondNumber: UITextField!
+    
+    @IBOutlet weak var lblAddAnswer: UILabel!
+    
+    // multiple outlet
+    @IBOutlet weak var txtMulNum1: UITextField!
+    @IBOutlet weak var txtMulNum2: UITextField!
+    @IBOutlet weak var lblMulRes: UILabel!
+  
+    //division outlet
+    @IBOutlet weak var txtDivNum1: UITextField!
+    
+    @IBOutlet weak var txtDivNum2: UITextField!
+    
+    @IBOutlet weak var lblDivRes: UILabel!
+    
+    
+    // add var
+    var additionFirstNumber = ""
+    var additionSecondNumber = ""
+    var resultAdd = 0.0
+    
+    // mul var
+    var mulFirstNumber = ""
+    var mulSecondNumber = ""
+    var resultMul = 0.0
+    
+    // div var
+    var divFirstNumber = ""
+    var divSecondNumber = ""
+    var resDiv = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+//        self.txtAddFirstNumber.delegate = self
+//        self.txtAddSecondNumber.delegate = self
+   
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +57,64 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func btnAddAction(sender: UIButton) {
+        additionFirstNumber = txtAddFirstNumber.text!
+        additionSecondNumber = txtAddSecondNumber.text!
+        
+        let resultAdd = Float(additionFirstNumber)! + Float(additionSecondNumber)!
+        
+        lblAddAnswer.text = "\(resultAdd)"
+    }
+    
+    
+    @IBAction func btnMultiAction(sender: UIButton) {
+        mulFirstNumber = txtMulNum1.text!
+        mulSecondNumber = txtMulNum2.text!
+        
+        let res = Float(mulFirstNumber)! * Float(mulSecondNumber)!
+        
+        lblMulRes.text = "\(res)"
+    }
+    
+    
+    @IBAction func btnDivAction(sender: UIButton) {
+        divFirstNumber = txtDivNum1.text!
+        divSecondNumber = txtDivNum2.text!
+        
+        let res = Float(divFirstNumber)! / Float(divSecondNumber)!
+        
+        lblDivRes.text = "\(res)"
+    }
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    @IBAction func btnClearAddAction(sender: UIButton) {
+        txtAddSecondNumber.text = ""
+        txtAddFirstNumber.text = ""
+        lblAddAnswer.text = "Answer"
+        
+    }
+    
+    @IBAction func btnClearMulAction(sender: UIButton) {
+        txtMulNum1.text = ""
+        txtMulNum2.text = ""
+        lblMulRes.text = "Answer"
+    }
+    
+    @IBAction func btnClearDiv(sender: UIButton) {
+        txtDivNum1.text = ""
+        txtDivNum2.text = ""
+        lblDivRes.text = "Answer"
+    }
+    
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+//        txtAddSecondNumber.resignFirstResponder()
+//        txtAddFirstNumber.resignFirstResponder()
+        
+        return true;
+    }
+    
 
 }
 
--- Calculator/Calculator/SecondViewController.swift
+++ Calculator/Calculator/SecondViewController.swift
@@ -8,8 +8,18 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var txtSubNum1: UITextField!
+    
+    @IBOutlet weak var txtDivNum2: UITextField!
+    
+    @IBOutlet weak var lblSubRes: UILabel!
+    
+    var num1 = ""
+    var num2 = ""
+    var res: Float = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +29,30 @@ class SecondViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    @IBAction func txtSubAction(sender: UIButton) {
+        num1 = txtSubNum1.text!
+        num2 = txtDivNum2.text!
+        
+        res = Float(num1)! - Float(num2)!
+        
+        lblSubRes.text = "\(res)"
+    }
 
+    @IBAction func btnClearAction(sender: UIButton) {
+        txtSubNum1.text = ""
+        txtDivNum2.text = ""
+        lblSubRes.text = "Answer"
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        return true
+    }
 
 }
 
