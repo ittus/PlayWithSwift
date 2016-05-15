ac8eb81ae64f885a0c1c4aca8b8da0f51bf945bb
ittus <vuminhthang.cm@gmail.com>
2016-05-15 22:38:47 +0800

    finish tapper game

--- Tapper/Tapper/ViewController.swift
+++ Tapper/Tapper/ViewController.swift
@@ -8,8 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    // Properties
+    var maxTap = 0
+    var currentTap = 0
+    
+    // Outlet
+    @IBOutlet weak var ivLogo: UIImageView!
+    
+    @IBOutlet weak var btnPlay: UIButton!
+    
+    @IBOutlet weak var txtInput: UITextField!
+    
+    
+    @IBOutlet weak var btnStart: UIButton!
+    
+    
+    @IBOutlet weak var lblCountRes: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +38,61 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func btnPlayACTION(sender: UIButton) {
+
+        
+        if txtInput.text != nil && txtInput.text != "" {
+            
+            ivLogo.hidden = true
+            btnPlay.hidden = true
+            txtInput.hidden = true
+            
+            btnStart.hidden = false
+            lblCountRes.hidden = false
+            
+            maxTap = Int(txtInput.text!)!
+            currentTap = 0
+            
+            updateResult()
+        }
+    }
+    
+    @IBAction func btnStarAction(sender: UIButton) {
+        currentTap = currentTap + 1
+        
+        if (isGameOver()) {
+            restartGame()
+        } else {
+            updateResult()
+        }
+        
+    }
+    
+    func isGameOver() ->Bool {
+        if currentTap >= maxTap {
+            return true
+        }
+        return false
+    }
+    
+    func restartGame() {
+        
+        ivLogo.hidden = false
+        btnPlay.hidden = false
+        txtInput.hidden = false
+        txtInput.text = ""
+        
+        btnStart.hidden = true
+        lblCountRes.hidden = true
+    }
+    
+    func updateResult() {
+        lblCountRes.text = "\(currentTap) taps"
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 
 }
 
