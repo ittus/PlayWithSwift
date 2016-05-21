d196a69d9ff80d9104e8206b57d8fb7f367eb0c7
ittus <vuminhthang.cm@gmail.com>
2016-05-21 15:36:43 +0800

    set up calculator project

---
+++ Retro-Calculator/Retro-Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Retro-Calculator
+//
+//  Created by Minh Thang Vu on 5/21/16.
+//  Copyright © 2016 Minh Thang Vu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
