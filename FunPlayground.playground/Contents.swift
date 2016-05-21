3c9fb59404b278228034152c853cacad9c605a68
ittus <vuminhthang.cm@gmail.com>
2016-05-21 15:10:59 +0800

    Shoes Store app

--- FunPlayground.playground/Contents.swift
+++ FunPlayground.playground/Contents.swift
@@ -2,47 +2,55 @@
 
 import UIKit
 
-var str = "Hello, playground"
-print(str)
+//var str = "Hello, playground"
+//print(str)
+//
+//// new loop syntax
+//
+//for x in 0..<50 {
+//    print(x)
+//}
+//
+//
+//// big number
+//var bigNumber = 999999999999999
+//print(bigNumber + 1)
+//
+//
+//// pass param
+//var amount = 10
+//
+//func testCount(var amount: Double) {
+//    amount = 11
+//}
+//
+//testCount(Double(amount))
+//print(amount)
+//
+//// func define
+//
+//func add(firstNum: Double, secondNum: Double) -> Double {
+//    return firstNum + secondNum
+//}
+//
+//func sub(firstNum: Double, secondNum: Double) -> Double {
+//    return firstNum - secondNum
+//}
+//
+//func multi(firstNum: Double, secondNum: Double) -> Double {
+//    return firstNum * secondNum
+//}
+//
+//func div(firstNum: Double, secondNum: Double) -> Double {
+//    return firstNum / secondNum
+//}
+//
+//div(100.0, secondNum: 2.0)
 
-// new loop syntax
+var top3Color = [String](count: 3, repeatedValue: "Boring Brown")
 
-for x in 0..<50 {
-    print(x)
-}
+top3Color[0] = "Blue"
 
-
-// big number
-var bigNumber = 999999999999999
-print(bigNumber + 1)
+print(top3Color)
 
 
-// pass param
-var amount = 10
-
-func testCount(var amount: Double) {
-    amount = 11
-}
-
-testCount(Double(amount))
-print(amount)
-
-// func define
-
-func add(firstNum: Double, secondNum: Double) -> Double {
-    return firstNum + secondNum
-}
-
-func sub(firstNum: Double, secondNum: Double) -> Double {
-    return firstNum - secondNum
-}
-
-func multi(firstNum: Double, secondNum: Double) -> Double {
-    return firstNum * secondNum
-}
-
-func div(firstNum: Double, secondNum: Double) -> Double {
-    return firstNum / secondNum
-}
-
-div(100.0, secondNum: 2.0)
