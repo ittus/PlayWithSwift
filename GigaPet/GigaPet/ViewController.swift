5928b36274ce4a5d9a88f2eb44e4b3c030186d73
ittus <vuminhthang.cm@gmail.com>
2016-06-06 00:22:09 +0800

    start Giga Pet project

---
+++ GigaPet/GigaPet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GigaPet
+//
+//  Created by Minh Thang Vu on 6/6/16.
+//  Copyright © 2016 Minh Thang Vu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
