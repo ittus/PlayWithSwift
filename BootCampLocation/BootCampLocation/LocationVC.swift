152dd7f878762a2677c3bac3bab75bc0fac6f576
ittus <vuminhthang.cm@gmail.com>
2016-06-26 12:17:38 +0800

    request user location and zoom map to it

--- BootCampLocation/BootCampLocation/LocationVC.swift
+++ BootCampLocation/BootCampLocation/LocationVC.swift
@@ -9,16 +9,25 @@
 import UIKit
 import MapKit
 
-class LocationVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class LocationVC: UIViewController, UITableViewDelegate, UITableViewDataSource, MKMapViewDelegate {
     
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var map: MKMapView!
     
+    let regionRadius : CLLocationDistance = 1000
+    
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        map.delegate = self
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
+    override func viewDidAppear(animated: Bool) {
+        checkLocationStatus()
+    }
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         return UITableViewCell()
     }
@@ -33,6 +42,25 @@ class LocationVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         
+    }
+    
+    func checkLocationStatus() {
+        if CLLocationManager.authorizationStatus() == .AuthorizedWhenInUse {
+            map.showsUserLocation = true
+        } else {
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+    
+    func zoomToUserLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, regionRadius*2, regionRadius*2)
+        map.setRegion(coordinateRegion, animated: true)
+    }
+    
+    func mapView(mapView: MKMapView, didUpdateUserLocation userLocation: MKUserLocation) {
+        if let loc = userLocation.location {
+            zoomToUserLocation(loc)
+        }
     }
 
 
