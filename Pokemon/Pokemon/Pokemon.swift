6514e51597972916d53a8b69728349a8b1e12006
ittus <vuminhthang.cm@gmail.com>
2016-07-02 23:14:22 +0800

    add pokemon class

---
+++ Pokemon/Pokemon/Pokemon.swift
@@ -0,0 +1,28 @@
+//
+//  Pokemon.swift
+//  Pokemon
+//
+//  Created by Minh Thang Vu on 7/2/16.
+//  Copyright © 2016 Minh Thang Vu. All rights reserved.
+//
+
+import Foundation
+
+
+class Pokemon {
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId 
+    }
+}
