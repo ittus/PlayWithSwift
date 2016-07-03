b4a781904e0c8051854e73730fbdadf57c93026c
ittus <vuminhthang.cm@gmail.com>
2016-07-03 17:58:56 +0800

    change pokemon model and add pod file

--- Pokemon/Pokemon/PokemonDetailVC.swift
+++ Pokemon/Pokemon/PokemonDetailVC.swift
@@ -13,9 +13,27 @@ class PokemonDetailVC: UIViewController {
     var pokemon: Pokemon!
     
     @IBOutlet weak var lblName: UILabel!
+    @IBOutlet weak var imgMain: UIImageView!
+    
+    @IBOutlet weak var lblDescription: UILabel!
+    
+    @IBOutlet weak var lblType: UILabel!
+    @IBOutlet weak var lblDefense: UILabel!
+    @IBOutlet weak var lblHeight: UILabel!
+    
+    @IBOutlet weak var lblPokedexId: UILabel!
+    @IBOutlet weak var lblWeight: UILabel!
+    @IBOutlet weak var lblBaseAttack: UILabel!
+    
+    @IBOutlet weak var lblEvolution: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         lblName.text = pokemon.name
+    }
+    
+    @IBAction func btnBackPressed(sender: UIButton) {
     }
     
     
